1331340f3bc6b65dffabcd0a3b3601a7efe0f053
JimmyBonez <complexsion360@gmail.com>
2016-08-05 23:23:11 -0700

    It maybe working

---
+++ root/root.config.js
@@ -0,0 +1,27 @@
+(function () {
+    angular
+        .module('root')
+        .config(['$routeProvider', RootRouter]);
+
+    RootRouter.$inject = ['$routeProvider'];
+
+    function RootRouter($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'root/root.index.html',
+                controller: 'Root',
+                controllerAs: 'vm'
+            }).when('/code', {
+            templateUrl: 'code.html',
+            controller: '',
+            controllerAs: 'vm'
+        }).when('/talk', {
+            templateUrl: 'talk.html',
+            controller: '',
+            contrllerAs: 'vm'
+        }).otherwise({
+            redirectTo: '/'
+        });
+    }
+})();
+//a
